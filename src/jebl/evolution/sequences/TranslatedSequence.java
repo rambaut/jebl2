9241b03a8a197c1d67e5586b0a75dcfcb4ef406a
rambaut <rambaut@gmail.com>
2018-02-12 22:59:23 +0000

    Added optional reading frame to TranslatedSequence

--- src/jebl/evolution/sequences/TranslatedSequence.java
+++ src/jebl/evolution/sequences/TranslatedSequence.java
@@ -1,20 +1,34 @@
 package jebl.evolution.sequences;
 
 /**
- * @author rambaut
- *         Date: Jul 27, 2005
- *         Time: 12:48:31 AM
+ * @author Andrew Rambaut
  */
 public class TranslatedSequence extends FilteredSequence {
 
+	/**
+	 * Create a TranslatedSequence from a source codon or nucleotide sequence
+	 * @param source
+	 * @param geneticCode
+	 */
 	public TranslatedSequence(Sequence source, GeneticCode geneticCode) {
+		this(source, geneticCode, 1);
+	}
+
+	/**
+	 * Create a TranslatedSequence from a source codon or nucleotide sequence
+	 * @param source
+	 * @param geneticCode
+	 * @param frame indexed from 1
+     */
+	public TranslatedSequence(Sequence source, GeneticCode geneticCode, int frame) {
 		super(source);
 
 		this.geneticCode = geneticCode;
+		this.frame = frame;
 	}
 
 	protected State[] filterSequence(Sequence source) {
-		return jebl.evolution.sequences.Utils.translate(source.getStates(), geneticCode);
+		return jebl.evolution.sequences.Utils.translate(source.getStates(), geneticCode, frame);
 	}
 
     /**
@@ -25,5 +39,6 @@ public class TranslatedSequence extends FilteredSequence {
     }
 
 	private final GeneticCode geneticCode;
+	private final int frame;
 
 }
